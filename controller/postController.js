d5f3903dc094bbae721ab4ed6f87347f4cab86c7
Nick0086 <nikunjkalathiya86@gmail.com>
2024-02-07 19:02:25 +0530

    ct

--- controller/postController.js
+++ controller/postController.js
@@ -18,7 +18,7 @@ exports.createPost = async (req, res) => {
         console.log("FILE", req.file)
 
         // Path to the uploaded file on the server
-        const { url, public_id } = await uploadeCloudinary(req.file.path);
+        // const { url, public_id } = await uploadeCloudinary(req.file);
 
         const postData = await postmodel.create({
             ...req.body,
