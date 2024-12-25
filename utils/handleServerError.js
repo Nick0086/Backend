75ceeff7c276e6fad55095f59d9e34a7d2990653
Nick0086 <154456466+Nick0086@users.noreply.github.com>
2024-12-25 23:29:05 +0530

    show error

--- utils/handleServerError.js
+++ utils/handleServerError.js
@@ -1,4 +1,7 @@
 exports.handleServerError = (statu, res, error) => {
+
+    console.log("handleServerError",error)
+
     res.status(statu).json({
         status: false,
         message: "Internal server error",
